eb79cc02cf602a5b19d11f264214ede2df547d50
retrogav <gavbloodworth@gmail.com>
2021-01-03 21:48:10 +1100

    new ver more

--- Chapter 1/more01.c
+++ Chapter 1/more01.c
@@ -1,5 +1,11 @@
-/*  more01.c version 0.1
+/*  more01.c version 0.2
     read and print 24 lines then pause for a few special commands
+
+    Version history
+    0.1 first ver
+    0.2 changed user input to read from /dev/tty (keyboard) instead of from
+        the same stream as the data
+        
 */
 
 #include <stdio.h>
@@ -9,7 +15,7 @@
 #define LINELEN 512
 
 void do_more(FILE *);
-int see_more();
+int see_more(FILE *);
 
 int main(int ac, char *av[])
 {
@@ -41,47 +47,54 @@ void do_more(FILE *fp)
 {
     char line[LINELEN];
     int num_of_lines = 0;
-    int see_more();
+    int see_more(FILE *);
     int reply;
+    FILE *fp_tty;
 
-    while (fgets(line, LINELEN, fp))
+    fp_tty = fopen("/dev/tty", "r");                // command stream
+    if (fp_tty == NULL)                             // if open fails
     {
-        if (num_of_lines == PAGELEN)
+        exit(1);
+    }
+
+    while (fgets(line, LINELEN, fp))                // more input
+    {
+        if (num_of_lines == PAGELEN)                // full screen?
         {
-            reply = see_more();
-            if (reply == 0)
+            reply = see_more(fp_tty);               // pass the file
+            if (reply == 0)                         // user typed q
             {
                 break;
             }
-            num_of_lines -= reply;
+            num_of_lines -= reply;                  // reset count
         }
-        if (fputs(line, stdout) == EOF)
+        if (fputs(line, stdout) == EOF)             // show line
         {
-            exit(1);
+            exit(1);                                // or die
         }
-        num_of_lines++;
+        num_of_lines++;                             // count it
     }
 }
 
 /*  print message, wait for response, return # of lines to advance
-        q means no, space means yes, CR means one line
-    */
-int see_more()
+    q means no, space means yes, CR means one line
+*/
+int see_more(FILE *cmd)
 {
     int c;
 
-    printf("\033[7m more? \033[m");
-    while ((c = getchar()) != EOF)
+    printf("\033[7m more? \033[m");                 // reverse on a vt100
+    while ((c = getc(cmd)) != EOF)                  // reads from tty
     {
-        if (c == 'q')
+        if (c == 'q')  
         {
             return 0;
         }
-        if (c == ' ')
+        if (c == ' ')                               // space shows next page
         {
-            return PAGELEN;
+            return PAGELEN;                         // how many to show
         }
-        if (c == '\n')
+        if (c == '\n')                              // enter key shows 1 line
         {
             return 1;
         }
