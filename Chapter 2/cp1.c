e6e80cd445062a696a6a8777d396129d67923f22
retrogav <gavbloodworth@gmail.com>
2021-01-29 22:34:43 +1100

    loop breacket standard

--- Chapter 2/cp1.c
+++ Chapter 2/cp1.c
@@ -18,8 +18,7 @@
 
 void oops(char *s1, char *s2);
 
-int main(int ac, char *av[])
-{
+int main(int ac, char *av[]) {
     
     int in_fd;
     int out_fd;
@@ -27,47 +26,40 @@ int main(int ac, char *av[])
     char buf[BUFFERSIZE];
 
     // check args
-    if (ac != 3)
-    {
+    if (ac != 3) {
         fprintf(stderr, "usage: %s source destination\n", *av);
         exit(1);
     }
     
     // open files
-    if ((in_fd = open(av[1], O_RDONLY)) == -1)
-    {
+    if ((in_fd = open(av[1], O_RDONLY)) == -1) {
         oops("Cannot open", av[1]);
     }
     
-    if ((out_fd = creat(av[2], COPYMODE)) == -1){
+    if ((out_fd = creat(av[2], COPYMODE)) == -1) {
         oops("Cannot creat", av[2]);
     }    
 
     // copy files. read() returns 0 when the end of the file is reached
-    while ((n_chars = read(in_fd, buf, BUFFERSIZE)) > 0)
-    {
-        if (write(out_fd, buf, n_chars) != n_chars)
-        {
+    while ((n_chars = read(in_fd, buf, BUFFERSIZE)) > 0)  {
+        if (write(out_fd, buf, n_chars) != n_chars) {
             oops("Write error to", av[2]);
         }
     }
 
-    if (n_chars == -1)
-    {
+    if (n_chars == -1) {
         oops("Read error from", av[1]);
     }
 
     // close files
-    if (close(in_fd) == -1 || close(out_fd) == -1)
-    {
+    if (close(in_fd) == -1 || close(out_fd) == -1) {
         oops("Error closing files", "");
     }
 
     return 0;
 }
 
-void oops(char *s1, char *s2)
-{
+void oops(char *s1, char *s2) {
     fprintf(stderr, "Error: %s ", s1);
     perror(s2);
     exit(1);
